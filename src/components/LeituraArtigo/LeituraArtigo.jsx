2135dd5696d96f2ca9d593fc38ba0f4afeef7286
Jefferson <jeff.dev.br@gmail.com>
2024-07-04 23:05:12 -0300

    endereço da img

--- src/components/LeituraArtigo/LeituraArtigo.jsx
+++ src/components/LeituraArtigo/LeituraArtigo.jsx
@@ -6,7 +6,7 @@ const LeituraArtigo = ({data}) => {
         <section className="leitura-artigo">
         
             <div className="leia-content">
-                <img src={`../../../public/${data.imgPath}`} alt="Imagens Artigo" />
+                <img src={`/${data.imgPath}`} alt="Imagens Artigo" />
                 <h1>{data.title}</h1>
                 <span>{data.publicationDate} por {data.author}</span>
                 <p className="excerpt">{data.excerpt}</p>
