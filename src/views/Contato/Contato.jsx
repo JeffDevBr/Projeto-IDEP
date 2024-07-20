1a344602248ab42b9f9b7f2a39fe6134e9941f2e
Jefferson <jeff.dev.br@gmail.com>
2024-07-20 15:06:06 -0300

    contato

--- src/views/Contato/Contato.jsx
+++ src/views/Contato/Contato.jsx
@@ -10,10 +10,67 @@ const Contato = () => {
 
             <div className="layout-style-main-footer">
 
-                <div className="container">
-                    <h1>Contato</h1>
-                </div>
+                <div className="container contato">
+                    <div>
+                        <h1>Atendimento</h1>
 
+                        <h3>Televendas</h3>
+                        <p><span>Segunda</span> à <span>Sexta</span> das <span>8h00</span> às <span>19h00</span> e aos <span>Sábados</span> das <span>8h00</span> às <span>12h00</span>.</p>
+
+                        <h3>tendimento SAC</h3>
+                        <p><span>Segunda</span> à <span>Sexta</span> das <span>8h00</span> às <span>18h00</span> e aos <span>Sábados</span> das <span>8h00</span> às <span>12h00</span>.</p>
+                    </div>
+
+                    <div className="box-contato-fone">
+                        <div>
+                            <h1>Contato através do telefone</h1>
+                            <p>Para entrar em contato com nosso departamento de vendas, o cliente pode usar os telefones abaixo com DDD locais para economizar nas ligações interurbanas. Basta selecionar o estado em que se encontra.</p>
+                        </div>
+                        <div className="contato-fone">
+                            <div>
+                                <p>Rondônia</p>
+                                (69) 9999-9999
+                            </div>
+
+                            <div>
+                                <p>São Paulo-SP</p>
+                                (11) 9999-9999
+                            </div>
+                            <div>
+                                <p>Fortaleza-CE</p>
+                                (85) 9999-9999
+                            </div>
+                            <div>
+                                <p>loren-ipsu</p>
+                                (xx) 9999-9999
+                            </div>
+                            <div>
+                                <p>loren-ipsu</p>
+                                (xx) 9999-9999
+                            </div>
+                            <div>
+                                <p>loren-ipsu</p>
+                                (xx) 9999-9999
+                            </div>
+                            <div>
+                                <p>loren-ipsu</p>
+                                (xx) 9999-9999
+                            </div>
+                            <div>
+                                <p>loren-ipsu</p>
+                                (xx) 9999-9999
+                            </div>
+                            <div>
+                                <p>loren-ipsu</p>
+                                (xx) 9999-9999
+                            </div>
+                            <div>
+                                <p>loren-ipsu</p>
+                                (xx) 9999-9999
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Footer />
             </div>
 
--- src/views/Produtos/Produtos.jsx
+++ src/views/Produtos/Produtos.jsx
@@ -1,5 +1,7 @@
+import { CarouselComponent } from "../../components/CarouselComponent/CarouselComponent"
 import { Footer } from "../../components/Footer/Footer"
 import { Header } from "../../components/Header/Header"
+import { ProductContainer } from "../../components/ProductCard/ProductContainer"
 import "./index.scss"
 
 
@@ -10,14 +12,29 @@ const Produtos = () => {
 
             <div className="layout-style-main-footer">
                 <div className="container produtos">
-                    <div className="cards-produtos">
-                       
+
+                    <div className="space">
+                    <img src="baner3.png" alt="" />
                     </div>
-                    <div className="banner"> </div>
-                    <div>
-                       
+
+                  
+                        <div className="space">
+                            <ProductContainer />
+                        </div>
+
+                        <div className="space">
+                            <CarouselComponent />
+                        </div>
+
+                        <div className="space">
+
+                        <ProductContainer />
+                        </div>
+
+                  
+                    <div className="space">
+                        <img src="baner2.png" alt="" />
                     </div>
-                    <div className="banner"> </div>
                 </div>
 
                 <Footer />
